ed01ddfddbe15e393e14c3dc89a3f6b0c1d1b133
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-10-01 22:52:51 +0530

    Compleated the basic styling of the landing page.

--- frontend-ams/src/pages/landing.js
+++ frontend-ams/src/pages/landing.js
@@ -1,8 +1,19 @@
 import styles from "../app/page.module.css";
 
-export default function Landing(){
-    return(
-    <div className={styles.navbar}>
-        <span>Mentor Plus</span>
-    </div>
-)};
+export default function Landing() {
+    return (
+        <div>
+            <div className={styles.navbar}>
+                <span>Mentor Plus</span>
+            </div>
+            <div className={styles.landingContent}>
+                <div className={styles.mainContent}>
+                    <h1>Searching for <span>Best Mentors</span> in your field ?</h1>
+                </div>
+                <div className={styles.book}>
+                    <a href="#">Click here</a> to book your very own mentoring session now.
+                </div>
+            </div>
+        </div>
+    )
+};
