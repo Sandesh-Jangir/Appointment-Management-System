2918327f2546975562a6c62092bcddd7ef4deee9
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-10-04 22:03:34 +0530

    Created the endpoint to schedule a meeting.

---
+++ frontend-ams/src/pages/meet.js
@@ -0,0 +1,11 @@
+export default function ScheduleMeeting(){
+    return(
+        <container>
+            <title>Schedule Meeting</title>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+
+        </container>
+    )
+}
