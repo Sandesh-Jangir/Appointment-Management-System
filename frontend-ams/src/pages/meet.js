c7743ef1dff7a152de464b676fd5773277864b61
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-10-05 23:19:04 +0530

    Setted up cors and ensured smooth com between backend and frontend.

--- frontend-ams/src/pages/meet.js
+++ frontend-ams/src/pages/meet.js
@@ -1,10 +1,17 @@
-export default function ScheduleMeeting(){
-    return(
+export default function ScheduleMeeting() {
+
+    // Sample function to fetch data from the backend api.
+    const getData = async ()=>{
+        let data = await fetch('http://localhost:5500/fetchall')
+        let res = await data.json();
+        console.log(res)
+    };getData();
+    return (
         <container>
             <title>Schedule Meeting</title>
-        <div>
-            <h1>Hello World</h1>
-        </div>
+            <div>
+                <h1>Hello World</h1>
+            </div>
 
         </container>
     )
