b70ba330c60da2321351b4b494da9fa1aa28a07d
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-10-14 23:10:20 +0530

    Setted up the meet endpoint for further development.

--- frontend-ams/src/pages/meet.js
+++ frontend-ams/src/pages/meet.js
@@ -1,16 +1,26 @@
+import styles from "../styles/page.meeting.css";
 export default function ScheduleMeeting() {
 
     // Sample function to fetch data from the backend api.
     const getData = async ()=>{
-        let data = await fetch('http://localhost:5500/fetchall')
+        let data = await fetch('http://localhost:5000/fetchall')
         let res = await data.json();
         console.log(res)
     };getData();
     return (
         <container>
             <title>Schedule Meeting</title>
-            <div>
-                <h1>Hello World</h1>
+            <div className="flexContainer">
+                <form>
+                    <label>Name</label>
+                    <input/>
+                    <label>Email</label>
+                    <input/>
+                    <label>Subject</label>
+                    <textarea/>
+                </form>
+
+                <div className="calender"></div>
             </div>
 
         </container>
