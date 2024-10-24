f8ac9d821b63187b6c0cb1b62123114456664275
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-10-24 22:10:11 +0530

    Fecthed api and setted up dashboard for further development.

--- frontend-ams/src/pages/dashboard.js
+++ frontend-ams/src/pages/dashboard.js
@@ -1,9 +1,21 @@
 import { useRouter } from "next/router"
+import { useState } from "react";
 
-export default function Admin() {
+export default function  Admin() {
+    // const [meetings, setMeetings] = useState([])
+    var meetings=[]
+    const listAppointment = async ()=>{
+        const response = await fetch('http://localhost:5000/fetchall');
+        meetings = await response.json();
+        await meetings.map((meet)=>{
+            console.log (meet)
+        })
+    };listAppointment();
     return (
         <div>
             <h1>Hello World, This is admin dashboard</h1>
+            <div id="subjects">
+            </div>
         </div>
     )
 }
