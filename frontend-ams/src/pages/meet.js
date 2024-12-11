77202c390b20aee67199a2b51bb5b2125d1103a6
Sandesh-Jangir <sandeshatgithub@gmail.com>
2024-12-11 21:52:37 +0530

    Created the function to render the entire month. Now would be working on setting the occupy status while rendering.

--- frontend-ams/src/pages/meet.js
+++ frontend-ams/src/pages/meet.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "../styles/page.meeting.css";
+import style from "../styles/page.meeting.css";
 export default function ScheduleMeeting() {
     const [Meetings, setMeetings] = useState([])
     // Sample function to fetch data from the backend api.
@@ -10,12 +11,42 @@ export default function ScheduleMeeting() {
     };
     useEffect(()=>{
         getData();
+        const month = new Date(Date.now()).getMonth();
+        const year = new Date(Date.now()).getFullYear();
+        const renderMonth = (month,year)=>{
+            let result = [];
+            // Rendering the month eith exact dates and days.
+            const len = new Date(year, month, 0).getDate() +1;
+            for (let i = 1; i <= len; i++) {
+                let date = new Date(year, month, i)
+                let day = date.getDay();
+                let ndate = date.getDate();
+                let occupyStatus = false;
+                result.push({
+                    "day": day,
+                    "date": ndate,
+                    "occupied": occupyStatus
+                })
+            }
+            
+            return result;
+        };
+        // console.log(month)
+        var res = renderMonth(month, year)
     }, [])
+    let i = 0
     Meetings.map((meeting)=>{
-                    let date = new Date(meeting.date)
-                    let day = date.getDate()
-                    document.getElementById("calendar").innerHTML += `<div class="box">${day}</div>`
+        let meetDay = new Date(meeting.date)
+        let meetDate = meetDay.getDate()
+        if (meetDate == res[i].date) res[i].occupied = true;
+        i++;
     })
+    Meetings.map((meeting)=>{
+        let date = new Date(meeting.date)
+                    let day = date.getDate()
+                    document.getElementById("calendar").innerHTML += `<button value =  class="box">${day}</button>`
+    })
+
     return (
         <container>
             <title>Schedule Meeting</title>
